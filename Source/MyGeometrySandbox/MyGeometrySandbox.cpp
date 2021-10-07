8b23d8f253b1f5cc6d125ec7bfcbf8bca067ee78
Void7 <limen191100@gmail.com>
2021-10-07 19:26:23 +0300

    Create UE4 project and C++ actor
    Created blank UE4 project MyGeometrySandbox
    Created C++ actor BaseGeometryActor

---
+++ Source/MyGeometrySandbox/MyGeometrySandbox.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "MyGeometrySandbox.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, MyGeometrySandbox, "MyGeometrySandbox" );
